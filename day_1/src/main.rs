4498197f9cd7a62886c6a00dd6a98a9134d802a1
Benjamin <b.charnet@groupe-aen.info>
2022-12-01 23:04:07 +0100

    day_1 solution

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,26 @@
+fn find_largest_number<'a, T: Ord>(array: impl Iterator<Item = &'a T>) -> Option<&'a T> {
+    array.max()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input");
+    // Step 1: Get every elf definition
+    // Note: Each elf is separated by two line breaks
+    let it = input.split("\r\n\r\n");
+
+    // Step 2: get every line of the array as numbers
+    let it = it.map(|lines| {
+        lines
+            .split("\r\n")
+            .map(|string_num| u64::from_str_radix(string_num, 10).unwrap_or(0))
+    });
+
+    // Step 3: Add all calories from each elf together
+    let elves = it.map(|calories| calories.sum::<u64>());
+
+    // Step 4: Find elf with most calories
+    println!(
+        "The elf with the most calories has: {} calories",
+        elves.max().unwrap_or(0)
+    );
 }
