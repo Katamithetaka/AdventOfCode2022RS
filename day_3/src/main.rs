9a96fadfb1fed795768949e02390632cfb36e7bb
Benjamin <b.charnet@groupe-aen.info>
2022-12-03 15:38:29 +0100

    part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -51,52 +51,6 @@ fn part_1() {
 
 fn part_2() {
     println!("Part 2!");
-
-    // Input format:
-    // Each line HAS n characters from a - z or A - Z
-    // Each half of the string only contains one TYPE of character in common
-    // Each chunk of 3 lines contains only one letter in common
-    let input = include_str!("input");
-
-    // Step 1: Get each line of the input;
-    let lines = input.lines().collect::<Vec<_>>();
-
-    // Step 2: Get the array in chunks of 3
-    let rucksacks = lines.chunks(3);
-
-    // Step 3: Find the duplicate characters in each chunks
-    let badges = rucksacks.map(|it| {
-        let first: Vec<_> = it[0].chars().collect();
-        let second: Vec<_> = it[1].chars().collect();
-        let third: Vec<_> = it[2].chars().collect();
-
-        for i in 0..first.len() {
-            // Find if second string contains first[i]
-            let second_has_duplicate = second.contains(&first[i]);
-
-            // Find if third contains first[i] (only executes if second contains first[i])
-            let has_duplicate = second_has_duplicate && third.contains(&first[i]);
-
-            if has_duplicate {
-                let c = first[i] as i32;
-                if c >= { 'a' as i32 } && c <= { 'z' as i32 } {
-                    return c - { 'a' as i32 } + 1;
-                }
-                if c >= { 'A' as i32 } && c <= { 'Z' as i32 } {
-                    return c - { 'A' as i32 } + 27;
-                }
-            }
-        }
-
-        // Only gets here if the file is badly formatted
-        assert!(false, "Shouldn't be getting here.");
-        return 0;
-    });
-
-    // Step 4: Get the sum of all badges
-    let sum = badges.sum::<i32>();
-
-    println!("{}", sum);
 }
 
 fn main() {
