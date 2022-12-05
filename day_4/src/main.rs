1db128582a8da4519f283e66131316edec3d922e
Benjamin <b.charnet@groupe-aen.info>
2022-12-05 02:32:50 +0100

    day_4 part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -45,7 +45,68 @@ fn part_1() {
     println!("{}", sum)
 }
 
-fn part_2() {}
+fn part_2() {
+    println!("Part 2!");
+
+    let input = include_str!("input");
+
+    // Step 1: Get each line of the input;
+    let ranges = input.lines();
+
+    // Step 2: Get the score from each line;
+    let ranges_contained = ranges.map(|pair| {
+        // Parse the string
+        let pair = pair
+            .split(",")
+            .map(|sections| {
+                sections
+                    .split("-")
+                    .map(|section_str| section_str.parse::<u32>().unwrap())
+                    .take(2)
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        // Simple function that checks if one lap overlaps with another
+        // for simplicity sake, assume range_1 is smaller than range_2
+        // see min_max to understand what I mean with smaller.
+        let overlaps = |range_1: &(u32, u32), range_2: &(u32, u32)| {
+            range_1.0 + (range_1.1 - range_1.0) >= range_2.0
+        };
+
+        // returns (smaller_range, bigger_range)
+        let min_max = |range_1: (u32, u32), range_2: (u32, u32)| {
+            if range_1.0 < range_2.0 {
+                (range_1, range_2)
+            } else if range_1.0 > range_2.0 {
+                (range_2, range_1)
+            } else {
+                if range_1.1 < range_2.1 {
+                    (range_1, range_2)
+                } else {
+                    (range_2, range_1)
+                }
+            }
+        };
+
+        let pair_1 = (pair[0][0], pair[0][1]);
+        let pair_2 = (pair[1][0], pair[1][1]);
+        // get the min_max of the pair
+        let (pair_1, pair_2) = min_max(pair_1, pair_2);
+
+        // check if it overlaps
+        if overlaps(&pair_1, &pair_2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    // Step 3: Get the sum
+    let sum: u32 = ranges_contained.sum();
+
+    println!("{}", sum)
+}
 
 fn main() {
     part_1();
