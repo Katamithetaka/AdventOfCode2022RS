bd1190000bb7df26e1b07f6ac437adc7a3f5746a
Katamithetaka <katamithetaka@gmail.com>
2022-12-09 17:55:59 +0100

    Day 8 (#10)
    * Added useful comments + Day 2 code (refactored)
    
    * part 1
    
    * part 2
    
    * part 1
    
    * part 2
    
    * day_4 part 1
    
    * day_4 part 2
    
    * Refactored all previous days, and completed day_6, also pushed day_5, todo: add comments
    
    * Fixed line ending problems
    
    * Fixed last newline issue (hopefully)
    
    * Day_7 + a bit of refactoring
    
    * Day_8
    
    * Day_8

---
+++ day_8/src/test.rs
@@ -0,0 +1,16 @@
+#[allow(unused_imports)]
+use crate::{part_1, part_2};
+
+#[test]
+fn part_1_test() {
+    let input = common::get_demo_input().expect("Couldn't read input");
+
+    assert!(part_1(&input) == 21);
+}
+
+#[test]
+fn part_2_test() {
+    let input = common::get_demo_input().expect("Couldn't read input");
+
+    assert!(part_2(&input) == 8);
+}
